1b7cdc752dd6ca14dbe91c9f257eb48da9ebfd63
Prasantacharya <prasantacharyabusiness@gmail.com>
2021-10-13 11:45:44 -0400

    forgot to commit and push

--- debugging/bad.c
+++ debugging/bad.c
@@ -2,7 +2,9 @@
 
 int main(){
 // TODO: write buggy c file
-
+    char* text = "hello there,";
+    text[0] = "H";
+    printf("%s \n %s", text,"GENERAL KENOBE!");
 // example of a seg fault
     return 0;
 }
